15b5c1486d451e6c964af8792eb1f6165efc4e17
Thomas <tzhang12062004@gmail.com>
2024-10-15 15:57:07 +0200

    removed some spinner things

--- Visualizations.py
+++ Visualizations.py
@@ -19,36 +19,28 @@ st.title(":blue[Visualizations]")
 tab1, tab2, tab3, tab4= st.tabs(["Pairplot", "Correlation Heatmap", "Scatterplot", "Histogram"])
 
 with st.spinner("Loading visualizations..."):
-    with tab1: 
-        #with st.spinner('Loading pairplot. Please wait...'):
-            #pairplot
-            st.header('Pairplot')
-            st.pyplot(sns.pairplot(df))
-    with tab2: 
-        with st.spinner('Loading heatmap. Please wait...'):
-            #heatmap
-            st.header('Heatmap')
-            corr_matrix= df.corr()
-            fig, ax = plt.subplots(figsize=(10, 6))
-            sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax, linewidths=0.5)
-            st.pyplot(fig)
+    with tab1: #pairplot
+        st.header('Pairplot')
+        st.pyplot(sns.pairplot(df))
+    with tab2: #heatmap
+        st.header('Heatmap')
+        corr_matrix= df.corr()
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', ax=ax, linewidths=0.5)
+        st.pyplot(fig)
 
-    with tab3: 
-        with st.spinner('Loading scatterplot. Please wait...'):
-            #scatterplot
-            st.header("Scatterplot")
-            fig, ax = plt.subplots(figsize=(10, 6))
-            sns.scatterplot(data = df, x = "dew", y = "temp")
-            st.pyplot(fig)
+    with tab3: #scatterplot
+        st.header("Scatterplot")
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.scatterplot(data = df, x = "dew", y = "temp")
+        st.pyplot(fig)
 
-    with tab4: 
-        with st.spinner('Loading histogram. Please wait...'):
-            #histogram
-            st.header("Histogram")
-            fig, ax = plt.subplots(figsize=(10, 6))
-            sns.histplot(df['temp'], kde=True)
-            st.pyplot(fig)
-            # plt.title("Distribution of Temperatures")
+    with tab4: #histogram
+        st.header("Histogram")
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.histplot(df['temp'], kde=True)
+        st.pyplot(fig)
+        # plt.title("Distribution of Temperatures")
 
 
 
