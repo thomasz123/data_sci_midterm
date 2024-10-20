81a37a3f2a84707b6240fdb726d32942957d91b2
thomasz123 <94854167+thomasz123@users.noreply.github.com>
2024-10-20 15:38:58 +0000

    ahh

--- conclusion.py
+++ conclusion.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 st.title(":blue[Conclusion]")
 st.subheader("Data Quality")
-st.write("Our dataset set was pulled from a website that only allowed the download of 1000 colums o")
+st.write("Our dataset set was pulled from a website that only allowed the download of 1000
+colums o")
 st.subheader("Model Related Improvements")
 "more stuff about more stuff"
 
