c8eaa1240763e6b15486ae4a99bd0e51b4ed2697
thomasz123 <94854167+thomasz123@users.noreply.github.com>
2024-10-20 15:29:43 +0000

    ahh

--- conclusion.py
+++ conclusion.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title(":blue[Conclusion]")
 st.subheader("Thoughts on dataset")
 "stuff about stuff"
-st.subheader("Accuracy of Predictions")
+st.subheader("Model Related Improvements")
 "more stuff about more stuff"
 
 st.markdown("stuff")
