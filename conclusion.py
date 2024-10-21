c71d1146c8a0347e839acd37e555c491bba79fc2
thomasz123 <94854167+thomasz123@users.noreply.github.com>
2024-10-21 11:25:53 +0000

    stuffs

--- conclusion.py
+++ conclusion.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title(":blue[Conclusion]🔬")
 st.subheader("Data Quality")
-st.write("Our dataset set was pulled from a website that only allowed the download of 1000 columns. As a result, our models were trained on only 1000 days, equating to less than 3 years of data.")
+st.write("Our dataset set was pulled from a website that only allowed the download of 1000 columns. As a result, our models were trained on only 1000 days, equating to less than 3 years of data.  Ideally, we would have had much more data to have a more accurate model.")
 st.subheader("Model Related Improvements")
 "For the linear regression, not all variables are helpful to predict temperature. Ex: percipitation has a negative R2 value, which means the model predicts temperature with less accuracy than a constant value (horizontal line). However, the accuracy of the model's predictions improves as more and more variables are included.   "
 "For the logistic regression, the model predicts temperature with an accuracy of about 60%"
