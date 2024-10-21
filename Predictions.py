a81e4c9bf53534665c7c9c4ca925160b2724b3ac
thomasz123 <94854167+thomasz123@users.noreply.github.com>
2024-10-21 11:59:27 +0000

    stuffs

--- Predictions.py
+++ Predictions.py
@@ -115,6 +115,9 @@ with tab2:
         # Create confusion matrix for plotting the comparison between true labels and predictions
         cm = confusion_matrix(ylog_test, logprediction)
 
+        logprediction.shape
+        ylog_test.shape
+
 
         fig, ax = plt.subplots(figsize = (10,6))
         sns.heatmap(pd.DataFrame(cm), annot = True, cmap = "YlGnBu")
