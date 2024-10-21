304d31f25b70041d6cae6c84befd93562a1796d8
thomasz123 <94854167+thomasz123@users.noreply.github.com>
2024-10-21 13:36:37 +0000

    stuffs

--- conclusion.py
+++ conclusion.py
@@ -4,7 +4,7 @@ st.title(":blue[Conclusion]🔬")
 st.subheader("Data Quality")
 st.write("Our dataset set was pulled from a website that only allowed the download of 1000 columns. As a result, our models were trained on only 1000 days, equating to less than 3 years of data.  Ideally, we would have had much more data to have a more accurate model.")
 st.subheader("Model Related Improvements")
-"For the linear regression, not all variables are helpful to predict temperature. Ex: percipitation has a negative R2 value, which means the model predicts temperature with less accuracy than a constant value (horizontal line). However, the accuracy of the model's predictions improves as more and more variables are included.   "
-"For the logistic regression, the model predicts temperature with an accuracy of about 60%"
+"For the linear regression, not all variables are helpful to predict temperature. Ex: precipitation has a negative R2 value, which means the model predicts temperature with less accuracy than a constant value (horizontal line). However, the accuracy of the model's predictions improves as more and more variables are included.   "
+"For the logistic regression, we noticed that more variables does not necessarily mean a more accurate model. From this, we suggest removing some variables in order to create a more accurate value. "
 st.subheader("Next Steps")
 st.markdown("This is a very simple analysis of weather data.  Based on our temperature predictions using different variables, it's clear to see that our predictions became more accurate the more variables were involved.  There are also countless more qualitative variables that we were not able to use in our analysis.  Meteorology is an extremely complex field focused on weather forecasting, and their predictions are much more accurate than ours will be.")
